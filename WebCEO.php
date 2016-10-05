db3624d6cad00ddbc542e4a535c9df9141d601a7
icezyburg <icezyburg@users.noreply.github.com>
2016-10-05 17:42:37 +0700

    startup

---
+++ WebCEO.php
@@ -0,0 +1,83 @@
+<?php
+namespace rds\WebCeo;
+
+/**
+ * 
+ * @author Pattamakorn Samhadthai <icezyburg@gmail.com>
+ * @license MIT
+ * @version 1.0
+ *
+**/
+
+class WebCeo
+{
+
+    private $api_url = "https://online.webceo.com/api/";
+
+    private $_curl_handle = NULL;
+
+    const HTTP_METHOD_POST = 'POST';
+
+    public function get($key = NULL, $endpoint, array $condition = array())
+    {
+        if (!isset($key) || $key === NULL)
+            throw new \InvalidArgumentException('Invalid api key - make sure api_key is defined in the config array');
+
+        return $this->_makeRequest($key, $endpoint, $condition);
+    }
+
+    private function _makeRequest($key, $endpoint, $condition, $method = "POST")
+    {
+        $command = array("key" => $key, "method" => $endpoint, "data" => $condition);
+
+        $ch = $this->_getCurlHandle();
+
+        $options = array(
+            CURLOPT_CUSTOMREQUEST => strtoupper($method),
+            CURLOPT_URL => $api_url,
+            CURLOPT_POSTFIELDS => "json=".urlencode(json_encode($command)),
+            CURLOPT_RETURNTRANSFER => true
+        );
+
+        if (!empty($curl_options)) {
+            $options = array_replace($options, $curl_options);
+        }
+        if (isset($this->_config['curl_options']) && !empty($this->_config['curl_options'])) {
+            $options = array_replace($options, $this->_config['curl_options']);
+        }
+
+        curl_setopt_array($ch, $options);
+
+        $response = curl_exec($ch);
+
+        $this->_debug_info = curl_getinfo($ch);
+
+        if ($response === false) {
+            throw new \RuntimeException('Request Error: ' . curl_error($ch));
+        }
+
+        $response = json_decode($response, true);
+
+        if (isset($response['result']) && ($response['result'] < 200 || $response['result'] > 300)) {
+            throw new \RuntimeException('Request Error: ' . $response['errormsg'] . '. Raw Response: ' . print_r($response, true));
+        }
+
+        return $response;
+
+    }
+
+    protected function _getCurlHandle()
+    {
+        if (!$this->_curl_handle) {
+            $this->_curl_handle = curl_init();
+        }
+        return $this->_curl_handle;
+    }
+    
+    public function __destruct()
+    {
+        if ($this->_curl_handle) {
+            curl_close($this->_curl_handle);
+        }
+    }
+}
